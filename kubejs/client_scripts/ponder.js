3910c11af5a25cc6258d3f4ee21a06f0ad4757b8
Danny Le <DannyLeWasTaken@protonmail.com>
2022-07-04 02:24:28 -0400

    Increased time + Added coordinate for text

--- kubejs/client_scripts/ponder.js
+++ kubejs/client_scripts/ponder.js
@@ -28,13 +28,13 @@ onEvent("ponder.registry", (event)=>{
 
                 scene.world.setBlock([0,1,0], "minecraft:enchanting_table", true);
                 scene
-                    .text(stt(3), "Enchanting tables enchant items or blocks")
+                    .text(stt(4), "Enchanting tables enchant books, weapons, or armor.", [0,1,0])
                     .colored(PonderPalette.WHITE)
                     .placeNearTarget()
                     .attachKeyFrame();
                 
                 scene
-                    .showControls(stt(3), [0,2,0], "down")
+                    .showControls(stt(4), [0,2,0], "down")
                     .leftClick()
                     .withItem("book");
             }
