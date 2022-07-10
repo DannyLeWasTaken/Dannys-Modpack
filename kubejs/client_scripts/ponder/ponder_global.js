acbeb453a397ebb25fd0481afbcc6abb52254d2c
Danny Le <DannyLeWasTaken@protonmail.com>
2022-07-09 23:50:11 -0400

    Added documentation

--- kubejs/client_scripts/ponder/ponder_global.js
+++ kubejs/client_scripts/ponder/ponder_global.js
@@ -1,4 +1,11 @@
 class Block {
+    /**
+     * 
+     * @param {scene} scene 
+     * @param {util} util 
+     * @param {string} blockId 
+     * @param {Vector3} location 
+     */
     constructor(scene, util, blockId, location) {
         scene.world.setBlocks(location, blockId);
     }
