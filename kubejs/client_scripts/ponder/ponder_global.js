827af026b4f679b7fb0b4599c9990cc77ae08447
Danny Le <DannyLeWasTaken@protonmail.com>
2022-07-09 23:48:07 -0400

    Refactored into a classs system

--- kubejs/client_scripts/ponder/ponder_global.js
+++ kubejs/client_scripts/ponder/ponder_global.js
@@ -1,28 +1,5 @@
-/**
- * 
- * @param {int} second The amount of seconds
- * @returns int Returns seconds converted to ticks
- */
- function stt(second) {
-    return second * 20;
-};
-
-/**
- * Fades in blocks
- * @param {*} scene 
- * @param {*} util 
- * @param {Vector3} location 
- * @param {string} blockName 
- * @param {float} wait 
- * @param {float} waitTime 
- */
-function fadeInBlock(scene, util, location, blockName, wait, waitTime) {
-    wait = wait ?? true;
-    waitTime = waitTime ?? 15;
-    const topLocation = util.vector.topOf(location); 
-    scene.world.setBlocks(location, blockName);
-    let link = scene.world.showSection(location, Facing.DOWN);
-    //scene.world.replaceBlocks(topLocation, "minecraft:air", true);
-    //scene.world.moveSection(link, [0, -1, 0], 0);
-    scene.idle(waitTime);
-};
+class Block {
+    constructor(scene, util, blockId, location) {
+        scene.world.setBlocks(location, blockId);
+    }
+}
